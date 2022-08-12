58683b101b762331627ea01b72fc274d40227650
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-12 12:30:26 -0700

    1 fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,5 @@
 # installs flask using pip3
-package { 'python3':
-  ensure   => 'installed'
-}
-
-package { 'pip3':
-  require  => 'python3',
-  ensure   => latest,
-  provider => 'pip3',
-}
-
 package { 'flask':
-  require  => 'python3',
   name     => 'flask',
   ensure   => '2.1.0',
   provider => 'pip3'
