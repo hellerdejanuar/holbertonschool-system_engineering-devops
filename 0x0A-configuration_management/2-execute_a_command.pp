4a740e57635376e666e569aba687798ed8a26c48
hellerdejanuar <hellerdejanuar@gmail.com>
2022-08-12 13:29:08 -0700

    2 style

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # executes pkill to terminate a program
 exec { 'kill program':
-  command   => 'pkill killmenow',
-  path      => '/usr/bin'
+  command => 'pkill killmenow',
+  path    => '/usr/bin'
 }
