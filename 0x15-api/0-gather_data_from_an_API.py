16cbb0840586ad4fbe0d2d8c3311538dbb5d1d17
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-06 13:29:16 -0700

    0 add

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" API requests module """
+
+from sys import argv
+import requests
+
+if __name__ != "__main__":
+    raise Exception("Imported")
+
+
+# print("argv: " + argv[1])
+user_id = argv[1]
+n_tasks_total = 0
+n_tasks_done = 0
+
+user_tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                          .format(user_id)).json()
+user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(user_id)).json()['name']
+
+for task in user_tasks:
+    n_tasks_total += 1
+    if task['completed'] is True:
+        n_tasks_done += 1
+
+
+print("Employee {} is done with tasks({}/{}):"
+      .format(user_name, n_tasks_done, n_tasks_total))
+
+for task in user_tasks:
+    if task['completed'] is True:
+        print("\t {}".format(task['title']))
