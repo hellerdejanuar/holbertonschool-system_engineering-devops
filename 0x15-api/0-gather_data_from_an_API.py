76b80948f015575906b4a87b6da01731029d987c
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-06 13:46:36 -0700

    0 fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-""" API requests module """
+""" 
+    API requests module
+    this module fetches data from API 
+"""
+
 
 import requests
 from sys import argv
 
+if __name__ == "__main__":
 
-if __name__ != "__main__":
-    raise Exception("Imported")
+    user_id = argv[1]
+    n_tasks_total = 0
+    n_tasks_done = 0
 
-user_id = argv[1]
-n_tasks_total = 0
-n_tasks_done = 0
-
-user_tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+    user_tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
                           .format(user_id)).json()
-user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+    user_name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                          .format(user_id)).json()['name']
 
-for task in user_tasks:
-    n_tasks_total += 1
-    if task['completed'] is True:
-        n_tasks_done += 1
+    for task in user_tasks:
+        n_tasks_total += 1
+        if task['completed'] is True:
+            n_tasks_done += 1
 
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, n_tasks_done, n_tasks_total))
 
-print("Employee {} is done with tasks({}/{}):"
-      .format(user_name, n_tasks_done, n_tasks_total))
-
-for task in user_tasks:
-    if task['completed'] is True:
-        print("\t {}".format(task['title']))
+    for task in user_tasks:
+        if task['completed'] is True:
+            print("\t {}".format(task['title']))
