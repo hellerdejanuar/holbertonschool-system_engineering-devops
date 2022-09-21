8294e00cc6eed39da67fa7e8da3491e7e6ebc66c
Mauricio Heller <hellerdejanuar@gmail.com>
2022-09-21 18:58:02 -0300

    0 fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -13,7 +13,7 @@ def recurse(subreddit, hot_list=[], after=None):
                         allow_redirects=False)
 
     if resp.status_code != 200:
-        print('None')
+        print(None)
     else:
         data = resp.json().get('data').get('children')
         for elem in data:
