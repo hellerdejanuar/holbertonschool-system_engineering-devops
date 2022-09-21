81a50f4069856108d8ad2848157088ce3fb24745
Mauricio Heller <hellerdejanuar@gmail.com>
2022-09-21 17:11:13 -0300

    0 done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-""" API module """
+""" Reddit API module """
+
 
 def number_of_subscribers(subreddit):
+    """ Returns number of suscribers of a subreddit """
+    import requests
 
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    resp = requests.get(url, headers=headers)
+
+    return resp.json().get('data').get('subscribers') or 0
