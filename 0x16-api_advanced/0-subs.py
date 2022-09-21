91d1ebf4323cb9124b1fa2acd2111f5393ad77f3
Mauricio Heller <hellerdejanuar@gmail.com>
2022-09-21 18:58:30 -0300

    0 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,6 +8,6 @@ def number_of_subscribers(subreddit):
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers, allow_redirects=False)
 
     return resp.json().get('data').get('subscribers') or 0
