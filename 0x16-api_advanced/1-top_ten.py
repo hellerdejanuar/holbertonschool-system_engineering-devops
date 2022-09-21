c77e3d69e27e7982f48a9b2f43e3f7fe19fbedd9
Mauricio Heller <hellerdejanuar@gmail.com>
2022-09-21 17:34:43 -0300

    test

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,7 +2,7 @@
 """ Reddit API module """
 
 
-def number_of_subscribers(subreddit):
+def top_ten(subreddit):
     """ Returns number of suscribers of a subreddit """
     import requests
 
@@ -10,8 +10,10 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "Mozilla/5.0"}
     resp = requests.get(url, headers=headers)
 
-    data = resp.json().get('data').get('children') or 0
+    data = resp.json().get('data').get('children')
+    print(data)
     for count, elem in enumerate(data):
         if count >= 10:
             break
-        print(elem.get('title'))
+        print(elem.get(count).get('title'))
+    
