84cebd8ccd5d0f7b3d94147fcee2104b5a621ddc
hellerdejanuar <hellerdejanuar@gmail.com>
2022-09-21 21:05:43 +0000

    1 working

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,14 +6,16 @@ def top_ten(subreddit):
     """ Returns number of suscribers of a subreddit """
     import requests
 
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    resp = requests.get(url, headers=headers)
+    resp = requests.get(url, headers=headers, allow_redirects=False)
 
-    data = resp.json().get('data').get('children')
-    print(data)
-    for count, elem in enumerate(data):
-        if count >= 10:
-            break
-        print(elem.get(count).get('title'))
-    
+    if resp is None:
+        print('None')
+    else:
+        data = resp.json().get('data').get('children')
+        for count, elem in enumerate(data):
+            if count >= 10:
+                break
+            print(elem.get('data').get('title'))
+            
