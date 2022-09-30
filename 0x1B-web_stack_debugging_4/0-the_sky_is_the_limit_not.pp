b000db8224c901fc1ce613ecd0085698dd58d8fd
Mauricio Heller <73609560+hellerdejanuar@users.noreply.github.com>
2022-09-30 10:47:45 -0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,6 +3,7 @@
 # Change defaults file
 exec { 'fix-file':
   command => 'sed -i "s/ULIMIT.*/ULIMIT=\"-n 2048\"/" /etc/default/nginx',
+  command => "sed -i 's/worker_processes .*;/worker_processes 7;/g',
   path    => '/usr/local/bin/:/bin/'
 }
 
